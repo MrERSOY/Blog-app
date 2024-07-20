2495c00fa725cfb1a3bbddfca256e0c978484f8b
Çağatay ERSOY <cersoy@Cagatay-MacBook-Pro.local>
2024-07-20 15:14:32 +0300

    first day

---
+++ Components/BlogList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import BlogItem from "./Blogitem";
+import { blog_data } from "@/Assets/assets";
+
+function BlogList() {
+  return (
+    <div>
+      <div className="flex justify-center gap-6 my-10">
+        <button className="bg-black text-white py-1 px-4 rounded-sm">
+          All
+        </button>
+        <button>Technology</button>
+        <button>Startup</button>
+        <button>Lifestyle</button>
+      </div>
+      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+        {blog_data.map((item, index) => {
+          return (
+            <BlogItem
+              key={index}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+              category={item.category}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default BlogList;
