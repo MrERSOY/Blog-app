78224d3a7742d6f019ca7d675f7142aa0c91e326
Çağatay ERSOY <cersoy@Cagatay-MacBook-Pro.local>
2024-07-25 22:27:10 +0300

    fourth day

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -10,9 +10,19 @@ const LoadDB = async () => {
 
 LoadDB();
 
+// API Endpoint to get all blogs
 export async function GET(request) {
-  return NextResponse.json({ msg: "API Working" });
+  const blogId = request.nextUrl.searchParams.get("id");
+  if (blogId) {
+    const blog = await BlogModel.findById(blogId);
+    return NextResponse.json(blog);
+  } else {
+    const blogs = await BlogModel.find({});
+    return NextResponse.json({ blogs });
+  }
 }
+
+// API Endpoint For Uploading Blogs
 
 export async function POST(request) {
   const formData = await request.formData();
